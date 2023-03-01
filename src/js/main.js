455805822afb1fa3b2c961bd8caf8a196bc2d6f2
Igor Timonin <mut4nt@yandex.ru>
2023-03-01 19:46:58 +0300

    main layout & styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap';
+// Import our custom CSS
 import '../scss/styles.scss';
 
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
